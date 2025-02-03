23f86c037f6d3f6a0a8e8f276d319840ffe3184f
Jamie Paul <185669+jamiepaul@users.noreply.github.com>
2025-02-03 11:54:43 -0500

    add initial files

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      cssModules: true,
+      targets: browserslistToTargets(browserslist('>= 0.25%')),
+    },
+  },
+  build: {
+    cssMinify: 'lightningcss',
+  },
+});
