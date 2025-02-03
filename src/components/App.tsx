834fd574292155a4006ee9acf78440745bf31a0e
Jamie Paul <185669+jamiepaul@users.noreply.github.com>
2025-02-03 12:57:44 -0500

    setup base styles and markup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react';
-import reactLogo from '../assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header className={`${styles.container} ${styles.header}`}>
+        <h1>Patterns Game</h1>
+      </header>
+      <main className={styles.container}>Make grid here</main>
+      <footer className={`${styles.container} ${styles.footer}`}>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Icons by{' '}
+          <a target="_blank" href="https://icons8.com">
+            Icons8
+          </a>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </footer>
     </>
   );
 }
